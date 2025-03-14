026613315c1cbf2501d04927eba1060d81b80e3a
Alex <alexedwardjames09@gmail.com>
2025-03-15 07:30:09 +1030

    Basic setupt for CLI loop. No aditional files added yet

--- CentinalMain.c
+++ CentinalMain.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
+#define MAXINPUT 20
+
+void Help() {
+    printf("Centinal CLI Commands\n");
+    printf("1. Help\n");
+}
+
+
+int main() {
+    printf("Centinal CLI - Type 'help' for help.\n");
+
+    char input[MAXINPUT];
+    
+    // Main CLI loop
+    while(1) {
+        // Get commmand
+        printf("> ");
+        if (!fgets(input, MAX_INPUT, stdin)) {
+            break;
+        }
+        input[strcspn(input, "\n")] = 0;
+
+        if(strcmp(input, "help")) {
+            Help();
+        }
+    }
+}
