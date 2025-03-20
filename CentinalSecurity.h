865ac9339948c9753e16615161e586da5030b688
Alex <alexedwardjames09@gmail.com>
2025-03-20 13:40:54 +1030

    Error fixes

--- CentinalSecurity.h
+++ CentinalSecurity.h
@@ -11,117 +11,123 @@ FILE *whiteListFile;
 FILE *blackListFile;
 
 void SecurityHelp() {
-	printf("Centinal Security Commands\n");
-	printf("help - display this\n");
-	printf("exit - return to main CLI\n");
-	printf("scana - scan a directory\n");
-	printf("scanb - scan directory including subfolders\n");
+    printf("Centinal Security Commands\n");
+    printf("help - display this\n");
+    printf("exit - return to main CLI\n");
+    printf("scana - scan a directory\n");
+    printf("scanb - scan directory including subfolders\n");
 }
 
 void SecurityInit() {
-	// Open files, return if files are not found	
-	whiteListFile = fopen("WhiteList.txt", "r");
-	blackListFile = fopen("BlackList.txt", "r");
-	
-	if (whiteListFile == NULL) {
-		printf("WARNING, WHITELIST NOT FOUND\n");
-		return;
-	}
-	
-	else if (blackListFile == NULL) {
-		printf("WARNING, BLACKLIST NOT FOUND\n");
-		return;
-	}
+    // Open files, return if files are not found    
+    whiteListFile = fopen("WhiteList.txt", "r");
+    blackListFile = fopen("BlackList.txt", "r");
+    
+    if (whiteListFile == NULL) {
+        printf("WARNING, WHITELIST NOT FOUND\n");
+        return;
+    }
+    
+    else if (blackListFile == NULL) {
+        printf("WARNING, BLACKLIST NOT FOUND\n");
+        return;
+    }
 }
+
 // Secondary CLI system for when blacklisted keyword is found in a file
 void SecurityThreatHelp() {
-	printf("Centinal Security Threat Manager Commands\n");
-	printf("help - display this\n");
-	printf("exit / ignore - return to scan without action\n");
-	printf("purge - delete the file\n");
-	printf("whitelist - add file path to whitelist, will not be scanned again\n");
+    printf("Centinal Security Threat Manager Commands\n");
+    printf("help - display this\n");
+    printf("exit / ignore - return to scan without action\n");
+    printf("purge - delete the file\n");
+    printf("whitelist - add file path to whitelist, will not be scanned again\n");
 }
 
 void SecurityPurgeThreat(char filePath[100]) {
-	printf("Purging file\n");
-	
-	remove(filePath);
+    printf("Purging file\n");
+    
+    remove(filePath);
 }
-	
+    
 void SecurityThreatWhitelist(char filePath[100]) {
-	printf("White listing file\n");
+    printf("White listing file\n");
 
-	line[100];
-	FILE *whiteListAppend;
+    char line[100];
+    FILE *whiteListAppend;
 
-	whiteListAppend = fopen("WhiteList.txt", "a");
+    whiteListAppend = fopen("WhiteList.txt", "a");
 
-	strstr(filePath, "\n")
-	fprintf(whiteListAppend, filePath);
+    // Stripping newline from the file path
+    filePath[strcspn(filePath, "\n")] = 0;
+    fprintf(whiteListAppend, "%s\n", filePath);
 
-	printf("Next time file is scanned, %s, won't be considered a threat\n");
+    printf("Next time file is scanned, %s, won't be considered a threat\n");
 
-	fclose(whiteListAppend);
+    fclose(whiteListAppend);
 }
 
 void ThreatCLI(char filePath[100]) {
-	printf("Centinal Threat Manager. Type 'help' for command list\n");
+    printf("Centinal Threat Manager. Type 'help' for command list\n");
 
-	while (1) {
-		char input[100];
+    while (1) {
+        char input[100];
 
-		printf("Security>Threat> ");
+        printf("Security>Threat> ");
 
-		if (!fgets(input, sizeof(input), stdin)) {
-			return;
-		}
-		input[strcspn(input, "\n")] = 0;
+        if (!fgets(input, sizeof(input), stdin)) {
+            return;
+        }
+        input[strcspn(input, "\n")] = 0;
 
-		if (strcmp(input, "help") == 0) {
-			SecurityThreatHelp();
-		}
+        if (strcmp(input, "help") == 0) {
+            SecurityThreatHelp();
+        }
 
-		else if (strcmp(input, "exit") == 0 || strcmp(input, "ignore") == 0) {
-			return;
-		}
+        else if (strcmp(input, "exit") == 0 || strcmp(input, "ignore") == 0) {
+            return;
+        }
 
-		else if (strcmp(input, "purge" == 0)) {
-			SecurityPurgeThreat(filePath);
-		}
+        else if (strcmp(input, "purge") == 0) {
+            SecurityPurgeThreat(filePath);
+        }
 
-		else if (strcmp(input, "whitelist") == 0) {
-			SecurityThreatWhitelist(filePath);
-		}
+        else if (strcmp(input, "whitelist") == 0) {
+            SecurityThreatWhitelist(filePath);
+        }
 
-		else {
-			printf("Invalid command. Type 'help' for command list\n");
-		}
-	}
+        else {
+            printf("Invalid command. Type 'help' for command list\n");
+        }
+    }
 }
 
-void ScanFile(char filePath) {
-	FILE *file;
-	file = fopen(filePath, "r");
+void ScanFile(char filePath[100]) {
+    FILE *file;
+    file = fopen(filePath, "r");
 
-	char line[100]
+    char line[100];
 
-	while (fgets(line, sizeof(line), file)) {
-		for (int i = 0; i < 100; i++) {
+    while (fgets(line, sizeof(line), file)) {
+        fseek(blackListFile, 0, SEEK_SET);  // Reset file pointer to the beginning of blackListFile
 
-			char[30] blackListWord;
+        char blackListWord[30];
 
-			blackListWord = fgets(blackListWord, 100, blackListFile);
+        while (fgets(blackListWord, sizeof(blackListWord), blackListFile)) {
+            // Remove newline character from the blacklisted word
+            blackListWord[strcspn(blackListWord, "\n")] = 0;
 
-			if (strstr(line, blackListWord) != NULL) {
-				printf("WARNING, FILE WITH BLACKLISTED WORD FOUND\n");
-				printf("%s\n", filePath);
-				printf("Opening Centinal threat CLI");
-				ThreatCLI();
-			}
-		}
-	}
+            if (strstr(line, blackListWord) != NULL) {
+                printf("WARNING, FILE WITH BLACKLISTED WORD FOUND\n");
+                printf("%s\n", filePath);
+                printf("Opening Centinal threat CLI");
+                ThreatCLI(filePath);
+                fclose(file);
+                return;
+            }
+        }
+    }
 
-	return;
+    fclose(file);
 }
 
 void ScanDir(const char *dirPath) {
@@ -178,7 +184,6 @@ void ScanFull(const char *dirPath) {
 
         stat(fullPath, &path_stat);
 
-
         if (S_ISREG(path_stat.st_mode)) {
             ScanFile(fullPath);
         }
@@ -193,11 +198,11 @@ void ScanFull(const char *dirPath) {
 
 
 void SecurityLoop() {
-	char input[100];
-	
-	printf("Initialising security...");
-	SecurityInit();
-	
+    char input[100];
+    
+    printf("Initialising security...");
+    SecurityInit();
+    
     while (1) {
         printf("Security> ");
 
@@ -210,26 +215,26 @@ void SecurityLoop() {
             SecurityHelp();
         }
         
-        else if (strcmp(input, "exit")) {
-			// Close files before exiting
-			fclose(whiteListFile);
-			fclose(blackListFile);
-			
-			break;
-		}
+        else if (strcmp(input, "exit") == 0) {
+            // Close files before exiting
+            fclose(whiteListFile);
+            fclose(blackListFile);
+            
+            break;
+        }
 
-		else if (strcmp(input, "scana")) {
-			ScanDir();
-		}
+        else if (strcmp(input, "scana") == 0) {            
+            ScanDir(".");
+        }
 
-		else if (strcmp(input, "scanb")) {
-			ScanFull();
-		}
+        else if (strcmp(input, "scanb") == 0) {
+            ScanFull(".");
+        }
 
-		else {
-			printf("Unknown command. Type 'help' for a list of commands.\n");
-		}
-	}
+        else {
+            printf("Unknown command. Type 'help' for a list of commands.\n");
+        }
+    }
 }
 
 #endif
