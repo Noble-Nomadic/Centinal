478d17e6c165c085099b91f8c78b08058cea02b1
Alex <alexedwardjames09@gmail.com>
2025-04-03 09:20:39 +1030

    Started script reader
    Ignore the new compiled version and the testing file manager

---
+++ src/CentinalScripts.c
@@ -0,0 +1,65 @@
+#ifndef CentinalScripts_h
+#define CentinalScripts_h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+void ScriptHelp() {
+    printf("Centinal script reader commands\n");
+    printf("help - display this\n");
+    printf("exit - return to main CLI\n");
+    printf("read - execute a script\n");
+}
+
+void 
+
+void ReadScript() {
+    char fileName[100];
+    
+    printf("Enter file name: ");
+    if (!fgets(fileName, sizeof(fileName), stdin)) {
+        return;
+    }
+    fileName[strcspn(fileName, "\n")] = 0;
+    
+    FILE *file;
+    file = fopen(fileName, "r");
+    
+    char line[100]
+    
+    while (fgets(line, sizeof(line), file)) {
+        
+    }
+}
+
+
+void ScriptLoop() {
+    char input[100];
+
+    while (1) {
+        printf("Scripts> ");
+
+        if (!fgets(input, sizeof(input), stdin)) {
+            break;
+        }
+        input[strcspn(input, "\n")] = 0;
+
+        if (strcmp(input, "help") == 0) {
+            ScriptHelp();
+        }
+        
+        else if (strcmp(input, "exit") == 0) {
+            break;
+        }
+        
+        else if (strcmp(input, "read") == 0) {
+            ReadScript();
+        }
+    }
+    
+    return;
+}
+
+#endif
