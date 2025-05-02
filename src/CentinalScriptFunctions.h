6b5af583e795f52e2f764961f66a1bd19a3fab05
Alex <alexedwardjames09@gmail.com>
2025-05-02 13:40:40 +0930

    Fixed or something

--- src/CentinalScriptFunctions.h
+++ src/CentinalScriptFunctions.h
@@ -1,126 +1,130 @@
-#ifndef CentinalScripts_h
-#define CentinalScripts_h
+#ifndef CentinalScriptFunctions_h
+#define CentinalScriptFunctions_h
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "CentinalScripts.h"
 
-#include "CentinalScriptFunctions.h"
-
-
-void ScriptHelp() {
-    printf("Centinal script reader commands\n");
-    printf("help - display this\n");
-    printf("exit - return to main CLI\n");
-    printf("execute - execute a script\n");
-}
-
-void ExecuteLine(char line[]) {
-    // Remove newline character
-    line[strcspn(line, "\n")] = 0;
-
-    char mainWord[20];
-    char arg1[100];
-    int lineNumber;
-    char content[500];
-
-    if (sscanf(line, "%s", mainWord) != 1) return;
-
-    if (strcmp(mainWord, "new") == 0) {
-        if (sscanf(line, "%*s %s", arg1) == 1) {
-            ScriptMakeFile(arg1);
-        }
-    }
-
-    else if (strcmp(mainWord, "reset") == 0) {
-        if (sscanf(line, "%*s %s", arg1) == 1) {
-            ScriptResetFile(arg1);
-        }
-    }
-
-    else if (strcmp(mainWord, "delete") == 0) {
-        if (sscanf(line, "%*s %s", arg1) == 1) {
-            ScriptDeleteFile(arg1);
-        }
-    }
-
-    else if (strcmp(mainWord, "edit") == 0) {
-        if (sscanf(line, "%*s %s %[^\n]", arg1, content) == 2) {
-            ScriptEditFile(arg1, content);
-        }
-    }
-
-    else if (strcmp(mainWord, "line") == 0) {
-        if (sscanf(line, "%*s %s %d %[^\n]", arg1, &lineNumber, content) == 3) {
-            ScriptEditLine(arg1, lineNumber, content);
-        }
-    }
-
-    else if (strcmp(mainWord, "encrypt") == 0) {
-        char arg2[100];
-        int key;
-        if (sscanf(line, "%*s %s %s %d", arg1, arg2, &key) == 3) {
-            ScriptEncrypt(arg1, arg2, key);
-        }
-    }
-
-    else if (strcmp(mainWord, "decrypt") == 0) {
-        char arg2[100];
-        int key;
-        if (sscanf(line, "%*s %s %s %d", arg1, arg2, &key) == 3) {
-            ScriptDecrypt(arg1, arg2, key);
-        }
-    }
-
-    else if (strcmp(mainWord, "execute") == 0) {
-        if (sscanf(line, "%*s %s", arg1) == 1) {
-            ScriptExecuteScript(arg1);
-        }
-    }
-}
-
-void ReadScript(char fileName[100]) {
+void ScriptMakeFile(char fileName[]) {
+    FILE *file;
+    file = fopen(fileName, "w");
     
-    char line[600];
-    while (fgets(line, sizeof(line), file)) {
-        ExecuteLine(line);
-    }
+    fclose(file);
 
+    return;
+}
+
+void ScriptResetFile(char fileName[]) {
+    FILE *file;
+    file = fopen(fileName, "w");
+
+    fclose(file);
+
+    return;
+}
+
+void ScriptDeleteFile(char fileName[]) {
+    remove(fileName);
+    return;
+}
+
+void ScriptEditFile(char fileName[], char content[]) {
+    FILE *file;
+    file = fopen(fileName, "a");
+    
+    fprintf(file, "%s\n", content);
+    
     fclose(file);
 }
 
-void ScriptLoop() {
-    char input[100];
+void ScriptEditLine(const char *fileName, int lineNumber, const char *newLineData) {
+    char tempFileName[] = "temp.txt";
 
-    while (1) {
-        printf("Scripts> ");
-        if (!fgets(input, sizeof(input), stdin)) break;
-        input[strcspn(input, "\n")] = 0;
+    FILE *file;
+    file = fopen(fileName, "r");
 
-        if (strcmp(input, "help") == 0) {
-            ScriptHelp();
-        }
+    FILE *tempFile;
+    tempFile = fopen(tempFileName, "w");
 
-        else if (strcmp(input, "exit") == 0) {
-            break;
-        }
+    int currentLine = 0;
+    char line[100];
 
-        else if (strcmp(input, "execute") == 0) {
-            char fileName[100];
-    
-            printf("Enter file name: ");
-            if (!fgets(fileName, sizeof(fileName), stdin)) return;
-            fileName[strcspn(fileName, "\n")] = 0;
-
-            FILE *file = fopen(fileName, "r");
-            if (!file) {
-                printf("Failed to open file: %s\n", fileName);
-                return;
-            }
-
-            ReadScript(fileName);
+    while (fgets(line, sizeof(line), file)) {
+        currentLine++;
+        
+        if (currentLine == lineNumber) {
+            fprintf(tempFile, "%s\n", newLineData);
+        } 
+        
+        else {
+            fprintf(tempFile, "%s", line);
         }
     }
+
+    if (currentLine < lineNumber) {
+        fprintf(tempFile, "%s\n", newLineData);
+    }
+
+    fclose(file);
+    fclose(tempFile);
+
+    remove(fileName);               // delete the original
+    rename(tempFileName, fileName); // rename temp file to original
+
+
+    return;
 }
+
+void ScriptEncrypt(char inFileName[], char outFileName[], int key) {
+    FILE *inFile;
+    FILE *outFile;
+
+    inFile = fopen(inFileName, "r");
+    outFile = fopen(outFileName, "w");
+
+    char currentChar = fgetc(inFile);
+
+    while (currentChar != EOF) {
+        int ASCIITOAPPEND = currentChar;
+
+        // Encrypt and write to the file
+        fprintf(outFile, "%i ", ASCIITOAPPEND * key);
+
+        currentChar = fgetc(inFile);
+    }
+
+    fclose(inFile);
+    fclose(outFile);
+
+    return;
+}
+
+void ScriptDecrypt(char inFileName[], char outFileName[], int key) {
+    FILE *inFile;
+    FILE *outFile;
+    
+    inFile = fopen(inFileName, "r");
+    outFile = fopen(outFileName, "w");
+    
+    
+    int encryptedValue;
+
+    while (fscanf(inFile, "%i", &encryptedValue) != EOF) {
+        // Decrypt the current ASCII data and append it to the file
+        char decryptedChar = encryptedValue / key;
+        fputc(decryptedChar, outFile);
+    }
+
+    fclose(inFile);
+    fclose(outFile);
+
+    return;
+}
+
+void ScriptExecuteScript(char scriptname[]) {
+    FILE *file;
+    file = fopen(scriptname, "r");
+
+    ReadScript(file);
+}
+
 
 #endif
